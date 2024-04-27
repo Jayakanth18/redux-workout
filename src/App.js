0c1605e73f5e28fb62dc6bac03de76f76497d3eb
Jayakanth <jayakanth1812@gmail.com>
2024-04-27 16:28:47 +0530

    conter app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Counter from "./features/counter/Counter";
 
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <Counter />
     </div>
   );
 }
